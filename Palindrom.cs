3abfd00af7dd85798edc1994d5aa9c92d0bc72aa
Mahesh-Bharati <154223426+Mahesh-Bharati@users.noreply.github.com>
2023-12-19 08:41:47 +0530

    Add files via upload

---
+++ Palindrom.cs
@@ -0,0 +1,21 @@
+using System;
+class Palindrom
+{
+	public static void Main(string[])
+	{
+		int num=int.Parse(Console.ReadLine());
+		int num3=num;
+		while(num==0)
+		{
+			int num1=num%10;
+			num2=num2+num1*10;
+			num/=10;
+		}
+		if(num2==num3)
+		{
+			Console.WriteLine("Palindrom");
+		}
+		else
+			Console>WriteLine("Not Palindrom");
+	}
+}
